5d832089fa71104f261fc9fd1d637aa3c285b818
Sophia Edeki <sophiesse143@gmail.com>
2022-10-24 01:51:52 +0100

    navigation set up, used partials for templeting, rendered remaining pages using ejs layout

--- server.js
+++ server.js
@@ -18,10 +18,50 @@ app.use('/webfonts', express.static(__dirname + 'public/webfonts'))
 
 //set templete engine
 app.use(expressLayouts)
-app.set('layout', './layouts/full-width')
+app.set('layout', './layouts/home-templete')
 app.set('view engine', 'ejs')
 
 //navigation
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index', {title : 'Home Page'})
+})
+app.get('/about', (req, res) => {
+    res.render('about', {layout: './layouts/full-width',
+                         title: 'About Page'})
+})
+app.get('/songs', (req, res) => {
+    res.render('songs', {layout: './layouts/full-width',
+                         title: 'Discology/song Page'})
+})
+app.get('/song_list', (req, res) => {
+    res.render('song_list', {layout: './layouts/full-width',
+                              title: 'Discology/song_list Page'})
+})
+app.get('/add_playlist', (req, res) => {
+    res.render('add_playlist', {layout: './layouts/full-width',
+                                title: 'Playlist Page'})
+})
+app.get('/genres', (req, res) => {
+    res.render('genres', {layout: './layouts/full-width',
+                           title: 'Genres Page'})
+})
+app.get('/genres_single', (req, res) => {
+    res.render('genres_single', {layout: './layouts/full-width',
+                                 title: 'Genres_single Page'})
+})
+app.get('/download', (req, res) => {
+    res.render('download', {layout: './layouts/full-width',
+                            title: 'Download Page'})
+})
+app.get('/history', (req, res) => {
+    res.render('history', {layout: './layouts/full-width',
+                           title: 'History Page'})
+})
+app.get('/request_song', (req, res) => {
+    res.render('request_song', {layout: './layouts/full-width',
+                                title: 'Request_song Page'})
+})
+app.get('/contact', (req, res) => {
+    res.render('contact', {layout: './layouts/full-width',
+                            title: 'Contact Page'})
 })
