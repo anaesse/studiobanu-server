6903c5eb207d8e462d484e43e58ceaa155c2ce69
Sophia Edeki <sophiesse143@gmail.com>
2022-11-05 02:44:01 +0100

    updated

--- public/js/custom.js
+++ public/js/custom.js
@@ -468,3 +468,75 @@
             $(window).on("mousemove", moveCursor), $("a, .zoom-cursor").on("mouseenter", zoomCursor), $(".trigger-close").on("mouseenter", closeCursor), $("a, .zoom-cursor, .trigger-close, .trigger-plus").on("mouseleave", defaultCursor)
         })
 }(jQuery);
+
+
+// download/lyrics page
+var audio = document.getElementById('audio');
+var pauseBtn = document.getElementById('pauseBtn');
+var count = 0;
+let seek_slider = document.querySelector('.seek_slider');
+let volume_slider = document.querySelector('.volume_slider');
+let curr_time = document.querySelector('.current-time');
+// let total_duration = document.querySelector('.total-duration');
+
+let duration = 0;
+let myInterval; 
+
+function playPause(){
+    if(count == 0){
+        count = 1;
+        seek_slider.setAttribute("max", audio.duration)
+        audio.play();
+        pauseBtn.innerHTML = "&#9208;";
+        myInterval = setInterval(myTimer, 1000);
+        // start()
+    }else{
+        count = 0;
+        audio.pause();
+        seek_slider.setAttribute("max", audio.duration)
+
+        pauseBtn.innerHTML = "&#9658;";
+        myStopFunction();
+        // clearInterval(start)
+    }
+}
+
+
+function myTimer() {
+    console.log(audio.currentTime)
+    seek_slider.value = audio.currentTime
+    duration = audio.currentTime;
+}
+
+function myStopFunction() {
+  clearInterval(myInterval);
+}
+
+
+function stop(){
+//   playPause()
+  audio.pause();
+  audio.currentTime = 0;
+  seek_slider.setAttribute("min", "1")
+  pauseBtn.innerHTML = "&#9658;";
+  seek_slider.setAttribute("max", audio.duration)
+  count=0;
+//   clearInterval(start)
+}
+
+function seekTo(){
+    // let seekto = audio.duration * (seek_slider.value / 100);
+    audio.currentTime = seek_slider.value;
+}
+
+// const start = setInterval(()=> {
+//     const progressPercent = (audio.currentTime / audio.duration) * 100;
+//     console.log(progressPercent)
+//     seek_slider.value = `${progressPercent}%`;
+// },1000)
+function setVolume(){
+    audio.volume = volume_slider.value / 100;
+}
+
+
+
